1d100094d473451088ec38df3eae29b770035cbb
Akram Hossain <nayanbd17@gmail.com>
2025-03-18 15:01:32 +0600

    product stock and button ddesign

--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -18,10 +18,7 @@
                     </a>
                 </div>
                 <div class="right">
-                    <a target="_blank" href="https://api.whatsapp.com/send?phone=8801711728630" aria-label='whatsapp'
-                        class="bg-second text-white text-sm font-semibold py-2 px-4 rounded-md anim hover:bg-third xl:text-xl xl:py-2.5 xl:px-6 inline-flex items-center gap-x-2">
-                        <img src="/images/icons/whatsapp-icon.svg" alt="arrow" class="hidden xl:inline w-6 text-white fill-white"> হোয়াটস্যাপ করুন
-                    </a>
+                    @include('layouts.partials.whatsapp-button')
                 </div>
             </div>
         </div>
